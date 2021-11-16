ac7a65013b0e6b62244b465f65c8441bef066390
Bartek Antos <bartekantos@protonmail.com>
2021-11-16 22:41:33 +0100

    first commit

---
+++ Zadanie/Controllers/FormController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie.Models;
+
+namespace Zadanie.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Created", model);
+
+            }
+            else
+            {
+                return View(model);
+            }
+
+
+        }
+
+    }
+}
